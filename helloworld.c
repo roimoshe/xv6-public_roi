b1ca0bf2489961ebf394f154e8f328b40c39cfe2
Roi Moshe <rmoshe@apple.com>
2020-04-04 16:55:56 +0300

    wait test

--- helloworld.c
+++ helloworld.c
@@ -6,5 +6,14 @@ int
 main(int argc, char *argv[])
 {
     printf(1, "Hello World XV6\n");
+
+    int status;
+    int pid = fork();
+    if( pid == 0 ){
+      exit(4);
+    } else{
+      wait(&status);
+      printf(1, "child's exit status is : %d\n", status);
+    }
   exit(0);
 }
