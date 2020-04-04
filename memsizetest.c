8cb0b29304cd94ad5029088264805971ddcf3ffd
Roi Moshe <rmoshe@apple.com>
2020-04-04 13:58:50 +0300

    more fixes

--- memsizetest.c
+++ memsizetest.c
@@ -5,10 +5,10 @@
 int
 main(int argc, char *argv[])
 {
-    printf(1, "The process is using: %luB\n", memsize());
+    printf(1, "The process is using: %dB\n", memsize());
     void *ptr = malloc(1<<11);
-    printf(1, "The process is using: %luB\n", memsize());
+    printf(1, "The process is using: %dB\n", memsize());
     free(ptr);
-    printf(1, "The process is using: %luB\n", memsize());
+    printf(1, "The process is using: %dB\n", memsize());
   exit();
 }
